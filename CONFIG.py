049bb367610b97388a97cf5c3c3b3277b71dc7f1
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-09-22 15:45:46 +0200

    Create CONFIG.py

---
+++ CONFIG.py
@@ -0,0 +1,9 @@
+
+
+
+
+token = "YOUR_DISCORD_TOKEN_HERE"
+
+USERNAME = 'flask_username'  # Remplace par ton nom d'utilisateur
+PASSWORD = 'flask_password'  # Remplace par ton mot de passe
+web_port = 8766
